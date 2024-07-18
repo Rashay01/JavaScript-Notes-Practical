7b841647e0103485cd25fa1a6110cef57c0af72b
Rashay Daya <rashay.daya@sanlam.co.za>
2024-07-18 14:21:19 +0200

    nodemon and user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,20 +1,23 @@
-let user = {};
-
-exports.getAllUser = (req, res) =>{
+// controllers/userController.js
+let user = [];
+ 
+exports.getAllUser = (req, res) => {
     res.status(200).json(user);
 }
-
+ 
 exports.postUser = (req, res) => {
-    user = {...req.body};
+    user = [...user, {...req.body}];
     res.status(200).json(user);
 }
-
+ 
 exports.updateUser = (req, res) => {
-    user = {user,...req.body};
+    const { id } = req.params;
+    user[id] = req.body;
     res.status(200).json(user);
 }
-
+ 
 exports.deleteUser = (req, res) => {
-    user = {};
+    const { id } = req.params;
+    user.splice(id-1 , 1);
     res.status(200).json(user);
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const authMiddleware = require('../middleware/authMiddleware');
-const { updateUser, getAllUser, deleteUser, postUser} = require('../controllers/userController');
+const { getAllUser, postUser, updateUser, deleteUser } = require('../controllers/userController');
 const router = express.Router();
-
+ 
+ 
 router.get('/', authMiddleware, getAllUser);
 router.post('/', authMiddleware, postUser);
-router.put('/', authMiddleware, updateUser);
-router.delete('/', authMiddleware, deleteUser);
-
+router.put('/:id', authMiddleware, updateUser);
+router.delete('/:id', authMiddleware, deleteUser)
+ 
 module.exports = router;
