04ef94b3de0707f6a164d3eb0bd6c4683c6d6a52
Rashay01 <101921464+Rashay01@users.noreply.github.com>
2024-07-19 12:30:23 +0200

    different changes on Controller

--- controllers/formController.js
+++ controllers/formController.js
@@ -1,55 +1,88 @@
 //controllers/formController.js
 exports.saveFormData = (req, res) => {
-    const {name, email, message} = req.body;
-    const pool = req.pool;
-    console.log("Pool -", pool);
-    const sql = `INSERT INTO form_data (name, email, message) VALUES (?, ?, ?)`;
-    req.pool.query(sql, [name, email, message], (err, results) => {
-        console.log("Result: " + JSON.stringify(results));
-        if(err) {
-            console.error(err);
-            res.status(500).json({ error: 'Error saving form data' });
-            return;
-        }
-        res.json({
-            status: 'success',
-            data: {...req.body}
-        })  
-    })
-}
- 
+  const { name, email, message } = req.body;
+  const pool = req.pool;
+  const sql = `INSERT INTO form_data (name, email, message) VALUES (?, ?, ?)`;
+  req.pool.query(sql, [name, email, message], (err, results) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Error saving form data' });
+      return;
+    }
+    res.json({
+      status: 'success',
+      data: { ...req.body },
+    });
+  });
+};
+
 exports.updateFormData = (req, res) => {
-    const {id} = req.params;
-    const {name, email, message} = req.body;
-    const pool = req.pool;
-    const sql = "UPDATE form_data SET name = ?, email = ?, message = ? WHERE id = ?";
-    pool.query(sql, [name, email, message, id], (err, results) => {
-        if(err) {
-            console.error(err);
-            res.status(500).json({ error: 'Error updating form data' });
-            return;
-        }
-        res.json({
-            status: 'success',
-            message: 'Data updated successfully',
-            data: {...req.body}
-        });
-    })
-}
- 
+  const { id } = req.params;
+  const { name, email, message } = req.body;
+  const pool = req.pool;
+  const sql =
+    'UPDATE form_data SET name = ?, email = ?, message = ? WHERE id = ?';
+  pool.query(sql, [name, email, message, id], (err, results) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Error updating form data' });
+      return;
+    }
+    res.json({
+      status: 'success',
+      message: 'Data updated successfully',
+      data: { ...req.body },
+    });
+  });
+};
+
 exports.deleteFormData = (req, res) => {
-    const {id} = req.params;
-    const pool = req.pool;
-    const sql = "DELETE FROM form_data WHERE id = ?";
-    pool.query(sql, [id], (err, results) => {
-        if(err) {
-            console.error(err);
-            res.status(500).json({ error: 'Error deleting form data' });
-            return;
-        }
-        res.json({
-            status: 'success',
-            message: 'Data deleted successfully'
-        })
-    })
-}
+  const { id } = req.params;
+  const pool = req.pool;
+  const sql = 'DELETE FROM form_data WHERE id = ?';
+  pool.query(sql, [id], (err, results) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Error deleting form data' });
+      return;
+    }
+    res.json({
+      status: 'success',
+      message: 'Data deleted successfully',
+    });
+  });
+};
+
+exports.getSpecificFormData = (req, res) => {
+  const { id } = req.params;
+  const pool = req.pool;
+  const sql = 'SELECT * FROM form_data WHERE id = ?';
+  pool.query(sql, [id], (err, results) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Error selecting form data' });
+      return;
+    }
+    res.json({
+      status: 'success',
+      data: { ...req.body },
+    });
+  });
+};
+
+exports.getAllFormData = (req, res) => {
+  const pool = req.pool;
+  const sql = 'SELECT * FROM form_data';
+  pool.query(sql, (err, results) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Error selecting form data' });
+      return;
+    }
+    console.log(req.body);
+    res.json({
+      status: 'success',
+      data: { results },
+    });
+  });
+};
--- routes/formRoutes.js
+++ routes/formRoutes.js
@@ -1,10 +1,18 @@
 //routes/formRoutes.js
 const express = require('express');
-const { saveFormData, updateFormData, deleteFormData } = require('../controllers/formController');
+const {
+  saveFormData,
+  updateFormData,
+  deleteFormData,
+  getSpecificFormData,
+  getAllFormData,
+} = require('../controllers/formController');
 const router = express.Router();
- 
+
 router.post('/', saveFormData);
 router.put('/:id', updateFormData);
 router.delete('/:id', deleteFormData);
- 
+router.get('/:id', getSpecificFormData);
+router.get('/', getAllFormData);
+
 module.exports = router;
